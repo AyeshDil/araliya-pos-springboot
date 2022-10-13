ce999fd8da9d63059e17d046158d96cc315eae6f
AyeshDil <dilshanwma@gmail.com>
2022-10-14 01:36:36 +0530

    initial commit

---
+++ src/main/java/com/araliya/pos/araliyapos/AraliyaPosApplication.java
@@ -0,0 +1,13 @@
+package com.araliya.pos.araliyapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraliyaPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraliyaPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araliya/pos/araliyapos/AraliyaPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araliya.pos.araliyapos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraliyaPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
